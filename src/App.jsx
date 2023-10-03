3adcb7c7a6d8336573775c4b3a1572af11d33ed1
Sahajaddin Seikh <sahajaddinsk786@gmail.com>
2023-10-03 19:23:01 +0530

    Changes in tie message

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import GameBox from "./components/GameBox";
 import React, { useEffect, useState } from 'react';
 
 function App() {
+  // Initialize the state for the game board data, player's move, and winner
   const [data, setData] = useState(Array(9).fill(""));
   const [pMove, setPMove] = useState(false);
   const [winner, setWinner] = useState("");
 
   useEffect(() => {
+    // Check for a win condition using winLogic
     const win = winLogic(data);
     
     if (win[0]) {
+      // If there's a winner, set the winner state
       setWinner(win[1] === "x" ? "Player" : "Computer");
     } else {
+      // Check if the game is a tie by counting non-empty cells
       const emptyCount = data.filter((element) => element !== "").length;
       if (emptyCount === 9) {
         setWinner("Game is Tie!");
@@ -20,8 +24,10 @@ function App() {
     }
   }, [data]);
 
+  // Function to handle player's move
   const playerMove = (index) => {
     if (!data[index]) {
+      // Update the data array with the player's move and toggle player's turn
       const newData = [...data];
       newData[index] = "x";
       setData(newData);
@@ -29,11 +35,14 @@ function App() {
     }
   };
 
+  // Function for the computer's move
   const computerMove = () => {
+    // Find empty indexes for computer's move
     const emptyIndexes = data.map((element, index) => (!element ? index : null)).filter((index) => index !== null);
     const randomIndex = emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)];
     
     if (randomIndex !== undefined) {
+      // Update the data array with the computer's move and toggle player's turn
       const newData = [...data];
       newData[randomIndex] = "O";
       setData(newData);
@@ -42,6 +51,7 @@ function App() {
   };
 
   useEffect(() => {
+    // Trigger the computer's move when it's the computer's turn
     if (!pMove) {
       computerMove();
     }
@@ -54,21 +64,27 @@ function App() {
         <hr className="border-t-2 border-gray-400" />
         <div className="game_box flex-col relative left-10 justify-center mx-auto my-2">
           <div className="first-row flex">
+            {/* Render the GameBox component for the first row */}
             <GameBox index1={0} index2={1} index3={2} data={data} setClick={playerMove} />
           </div>
           <div className="second_row flex">
+            {/* Render the GameBox component for the second row */}
             <GameBox index1={3} index2={4} index3={5} data={data} setClick={playerMove} />
           </div>
           <div className="third_row flex">
+            {/* Render the GameBox component for the third row */}
             <GameBox index1={6} index2={7} index3={8} data={data} setClick={playerMove} />
           </div>
         </div>
         <hr className="border-t-2 border-gray-400" />
-        <h1 className={winner === "" ? 'hidden' : 'text-4xl'}>Winner is {winner}</h1>
+        {/* Display the winner if there is one */}
+        <h1 className={winner === "" ? 'hidden' : 'text-4xl'}>Winner : {winner}</h1>
         <div className="flex justify-center items-center my-2">
+          {/* Reset button to start a new game */}
           <button
             className="bg-yellow-600 py-1 rounded-lg px-2 text-white hover:cursor-pointer hover:bg-white hover:text-yellow-600"
             onClick={() => {
+              // Reset the game data and winner state
               setData(Array(9).fill(""));
               setWinner("");
             }}
